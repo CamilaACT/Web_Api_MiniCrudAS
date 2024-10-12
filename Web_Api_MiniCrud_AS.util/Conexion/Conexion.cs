ee263c8cf0cf811488c39de73bc8850f312e3404
CamilaPROCELEC <camilacabrera464@gmail.com>
2024-10-11 20:08:31 -0500

    Inicio Repositorio

---
+++ Web_Api_MiniCrud_AS.util/Conexion/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Api_MiniCrud_AS.util.Conexion
+{
+    public class Conexion
+    {
+
+        private static string servidor = @"LAPTOP_CAMILA\CAMILASQL";
+        private static string base_tip = "PRO_Web";
+        private static string usuario = "sa";
+        private static string password = "sasa";
+        private static string clav_priv = "dfaeiriecmvjhe3943dfadahyeu3456";
+        public static string CadenaConexion()
+        {
+
+            return "Data Source=" + servidor + ";Initial Catalog=" + base_tip + ";User ID=" + usuario + ";Password=" + password;
+        }
+
+
+    }
+}
